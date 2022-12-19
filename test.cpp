52ef93780cc58c6978cd43683f02e1e9522bc4d4
Mahdi <esmokes17@gmail.com>
2022-12-19 22:32:18 +0330

    possible to pass foldername as argv in test.cpp

--- test.cpp
+++ test.cpp
@@ -4,10 +4,20 @@
 
 #include "filed.h"
 
-const std::string FOLDER_NAME = ".file_hash";
+std::string folder_name = DEFAULT_FOLDER_NAME;
 
+bool dir_exists(std::string foldername)
+{
+#ifdef __win__
+    DWORD attribs = ::GetFileAttributesA(reinterpret_cast<LPCSTR>(foldername.data()));
+    if (attribs == INVALID_FILE_ATTRIBUTES) {
+        return false;
+    }
+    return (attribs & FILE_ATTRIBUTE_DIRECTORY);
+#endif  // __win__
+}
 
-std::vector<std::string> get_hash_from_folder(std::string foldername = FOLDER_NAME)
+std::vector<std::string> get_hash_from_folder(std::string foldername)
 {
     std::vector<std::string> hash_files = get_all_files_names_within_folder(foldername, "hash_*");
     std::vector<std::string> hashes;
@@ -23,7 +33,7 @@ std::vector<std::string> get_hash_from_folder(std::string foldername = FOLDER_NA
     return hashes;
 }
 
-std::string get_root_hash_from_folder(std::string foldername = FOLDER_NAME)
+std::string get_root_hash_from_folder(std::string foldername)
 {
     std::ifstream in;
     char str[HASH_SIZE + 1];
@@ -34,7 +44,7 @@ std::string get_root_hash_from_folder(std::string foldername = FOLDER_NAME)
     return str;
 }
 
-std::string get_index_from_folder(std::string foldername = FOLDER_NAME)
+std::string get_index_from_folder(std::string foldername)
 {
     std::ifstream in;
     std::string str;
@@ -44,17 +54,25 @@ std::string get_index_from_folder(std::string foldername = FOLDER_NAME)
     return str;
 }
 
-int main() {
-    std::vector<std::string> need_hash = get_hash_from_folder();
+int main(int argc, char * argv[])
+{
+    if (argc >= 2) folder_name = argv[1];
+    if (!dir_exists(folder_name)) {
+        std::cerr << "ERROR: Folder '" << folder_name << "' does not exist!" << std::endl;
+        exit(1);
+    }
+
+    std::vector<std::string> need_hash = get_hash_from_folder(folder_name);
     for (size_t i = 0 ; i < need_hash.size() ; i++) {
         std::cout << "Hash " << i << " '" << need_hash.at(i) << "' is read!" << std::endl;
     }
-    std::string root_hash = get_root_hash_from_folder();
+    std::string root_hash = get_root_hash_from_folder(folder_name);
     std::cout << "Root hash '" << root_hash << "' is read!" << std::endl;
-    std::string index = get_index_from_folder();
+    std::string index = get_index_from_folder(folder_name);
     std::cout << "index '" << index << "' is read!" << std::endl;
 
     merkle_ptr make_tree = MerkleTree::proof_of_exist(need_hash, DynamicBits::fromStr(index), root_hash);
+    std::cout << "**************************" << std::endl;
     if (make_tree == nullptr) std::cerr << "Not proof" << std::endl;
     else std::cout << "Proof root hash: " << make_tree->get_head()->get_hash() << std::endl;
     return 0;
